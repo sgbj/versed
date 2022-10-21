41b4e419c2a536fa0823846a85620418f7f900df
Peter Magnusson <me@kmpm.se>
2022-10-21 14:10:36 +0200

    check for interrupt
    - makes running interactive in docker much more pleasant

--- index.js
+++ index.js
@@ -63,3 +63,9 @@ app.post('/convert', upload.single('file'), function (req, res, next) {
 app.listen(3000, function () {
     console.log('Listening on port 3000');
 });
+
+
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+    process.exit();
+});
