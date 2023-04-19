e81f13a6d6ddc02479ce6d7ac26062e3677f35d6
cheikhgwane <cwane@bluesquarehub.com>
2023-04-19 15:04:05 +0000

    feat(Auth): fine-tuning

--- auth.js
+++ auth.js
@@ -1,18 +1,14 @@
 'use strict';
 
-import dotenv from 'dotenv';
-dotenv.config();
-
 export default (req, res, next) => {
     const authToken = req.headers.authorization;
     if (!authToken) {
-        res.status(401);
-        return res.send('Access Denied. Missing authorization header');
+        return res.status(401).json({ error: 'Access Denied. Missing authorization header' });
     }
 
-    if (authToken.replace('Bearer ', '') !== process.env.API_TOKEN) {
-        res.status(401);
-        res.send('Access Denied. Invalid bearer token supplied');
+    const auth = authToken.split(' ');
+    if (auth.length !== 2 || auth[0].toLowerCase() !== 'bearer' || auth[1] !== process.env.API_TOKEN) {
+        res.status(401).json({ error: 'Access Denied. Invalid bearer token supplied' });
     } else {
         next();
     }
