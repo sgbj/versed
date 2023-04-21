f98bac91522fdba7e96194cf2663c708d83038f3
cheikhgwane <cwane@bluesquarehub.com>
2023-04-21 11:59:50 +0000

    fix : whitelist readiness endpoint

--- auth.js
+++ auth.js
@@ -1,6 +1,10 @@
 'use strict';
 
 export default (req, res, next) => {
+    // readiness endpoint should be whitelisted
+    if (req.url.startsWith('/ready')) {
+        return next();
+    }
     const authToken = req.headers.authorization;
     if (!authToken) {
         return res.status(401).json({ error: 'Access Denied. Missing authorization header' });
